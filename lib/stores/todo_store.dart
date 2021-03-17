745834a87064f232f8438ed36d2b707edb09513f
Plinio Sandes dos Santos <ss.plinio@gmail.com>
2021-03-17 16:49:31 -0300

    first commit, app finished

---
+++ lib/stores/todo_store.dart
@@ -0,0 +1,18 @@
+import 'package:mobx/mobx.dart';
+
+part 'todo_store.g.dart';
+
+class TodoStore = _TodoStore with _$TodoStore;
+
+abstract class _TodoStore with Store {
+
+  final String title;
+
+  _TodoStore(this.title);
+
+  @observable
+  bool done = false;
+
+  @action
+  void toggleDone() => done = !done;
+}
